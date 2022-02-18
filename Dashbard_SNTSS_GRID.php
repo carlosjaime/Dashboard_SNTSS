a87a1fe1c99337437a36420f2de92664ebd1adb0
Carlos Jaime López Martínez <jaimtito682004@gmail.com>
2022-02-17 21:53:06 -0600

    Dashbard_SNTSS_GRID.php

---
+++ Dashbard_SNTSS_GRID.php
@@ -0,0 +1,136 @@
+ <!-- DASHBOARD PRINCIPAL-->
+
+
+  <!--<div  style="background-color: #0C1F99;">
+    
+<div class="container overflow-hidden" style="background-color: #0C1F99;">
+  <div class="row gy-5">
+    <div class="col-3" style="background-color:#C76E26;">
+      <a href="#Libros" data-bs-toggle="modal" data-bs-target="#libros"><div class="p-3 "><img src="../imagenes/books.ico.png" width="150px" height="150px"></div>
+      <div align="center"><b>Libros y  Utiles</b></div></a>
+    </div>
+    <div class="col-3" style="background-color:#C76E26;">
+       <a href="#Uniformes" data-bs-toggle="modal" data-bs-target="#uniformes"> <div class="p-3 " style=" padding-left: 20px;"><img src="../imagenes/uniforme.png.ico.png" width="170px" height="150px"></div>
+      <div align="center"><b>Uniformes</b></div></a>
+    </div>
+    <div class="col-3" style="background-color:#C76E26;">
+       <a href="#Reportes" data-bs-toggle="modal" data-bs-target="#reportes"><div class="p-3 "><img src="../imagenes/report_check_2.png" width="150px" height="150px"></div>
+      <div align="center"><b>Reportes</b></div></a>
+    </div>
+    <div class="col-3" style="background-color:#C76E26;">
+       <a href="#Alumnos" data-bs-toggle="modal" data-bs-target="#alumnos"><div class="p-3 "><img src="../imagenes/student_id_2.png" width="150px" height="150px"></div>
+      <div align="center"><b>Alumnos</b></div></a>
+    </div>
+     <div class="col-3" style="background-color:#C76E26;">
+      <a href="#Busqueda" data-bs-toggle="modal" data-bs-target="#busqueda"> <div class="p-3 "><img src="../imagenes/search.png" width="150px" height="150px"></div>
+      <div align="center"><b>Busqueda</b></div></a>
+    </div>
+    <div class="col-3" style="background-color:#C76E26;">
+      <a href="#Admin" data-bs-toggle="modal" data-bs-target="#adminp"><div class="p-3 "><img src="../imagenes/admin_panel.png" width="150px" height="150px"></div>
+      <div align="center"><b>Admin Panel</b></div></a>
+    </div>
+  </div>
+</div>
+
+</div>-->
+
+<div class="row row-cols-1 row-cols-md-3 mb-3 text-center" >
+ <a href="#Libros" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#libros">
+  <div class="col" >
+    <div class="card mb-4 rounded-3 shadow-sm">
+      <div class="card-header py-3">
+        <h4 class="my-0 fw-normal">LIBROS & ÚTILES</h4>
+      </div>
+      <div class="card-body" style="background-color:#C76E26;">
+
+
+        <img src="../imagenes/books.ico_comp.png" width="150px" height="150px">
+
+
+      </div>
+    </div>
+  </div></a>
+  <a href="#Uniformes2" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#uniformes2">
+    <div class="col">
+      <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-normal">UNIFORMES</h4>
+        </div>
+        <div class="card-body"  style="background-color:#C76E26;">
+
+
+          <img src="../imagenes/uniforme.png.ico_comp.png" width="190px" height="150px">
+
+        </div>
+      </div>
+    </div></a>
+    <a href="#Reportes"  style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#reportes">
+      <div class="col">
+       <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-normal">REPORTES</h4>
+        </div>
+        <div class="card-body"  style="background-color:#C76E26;">
+
+
+         <img src="../imagenes/report_check_2_comp.png" width="150px" height="150px">
+
+
+       </div>
+     </div>
+   </div></a>
+
+   <a href="#Alumnos" style="text-decoration: none;"  data-bs-toggle="modal" data-bs-target="#alumnos">
+    <div class="col">
+     <div class="card mb-4 rounded-3 shadow-sm">
+      <div class="card-header py-3">
+        <h4 class="my-0 fw-normal">ALUMNOS</h4>
+      </div>
+      <div class="card-body"  style="background-color:#C76E26;">
+
+
+       <img src="../imagenes/student_id_2_comp.png" width="150px" height="150px">
+
+
+     </div>
+   </div>
+ </div> </a>
+
+ <a href="#Busqueda" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#busqueda">
+  <div class="col">
+   <div class="card mb-4 rounded-3 shadow-sm">
+    <div class="card-header py-3">
+      <h4 class="my-0 fw-normal">BÚSQUEDA</h4>
+    </div>
+    <div class="card-body"  style="background-color:#C76E26;">
+
+     <img src="../imagenes/search_comp.png" width="150px" height="150px">
+
+   </div>
+ </div>
+</div></a>
+
+<a href="#Admin" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#adminp">
+  <div class="col">
+   <div class="card mb-4 rounded-3 shadow-sm">
+    <div class="card-header py-3">
+      <h4 class="my-0 fw-normal">ADMIN PANEL</h4>
+    </div>
+    <div class="card-body"  style="background-color:#C76E26;">
+
+
+     <img src="../imagenes/admin_panel_comp.png" width="150px" height="150px">
+     
+
+
+
+   </div>
+ </div>
+</div> </a>
+</div>
+
+
+
+</main>
+
+<!-- DASHBOARD PRINCIPAL-->
